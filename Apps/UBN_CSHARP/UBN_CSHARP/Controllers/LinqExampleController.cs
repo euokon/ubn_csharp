a359fee0489338f19729090accc61a99c29e90f6
Adetoyi Samson <hardetoy@gmail.com>
2023-02-18 15:10:24 +0100

    web_api

---
+++ Apps/UBN_CSHARP/UBN_CSHARP/Controllers/LinqExampleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using UBN_CSHARP.Models;
+
+namespace UBN_CSHARP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqExampleController : ControllerBase
+    {
+        [HttpGet("Customers")]
+        public IActionResult GetCustomers()
+        {
+            var customers = CustomerData.Customers.Select(a => new
+            {
+                CustomerId = a.CustomerId,
+                CustomerName = a.CustomerName,
+                CustomerType = a.CustomerType,
+                Gender = a.Gender,
+                CustomerRelationshipDate = a.CustomerRelationshipDate,
+            });
+
+            return Ok(customers);
+        }
+    }
+}
